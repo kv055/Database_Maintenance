db29a46798f387ce46a21c4afe14d9cd9f9c4897
Kilian Voss <kilian96@live.de>
2022-09-03 11:52:07 +0200

    loading env variables now inside the module instead of as params, new method to fetch OHLC does not work yet, need to figure out why

--- API_Connectors/AlpacaConnector.py
+++ API_Connectors/AlpacaConnector.py
@@ -1,18 +1,25 @@
+import os
+from dotenv import load_dotenv
+
 import alpaca_trade_api as tradeapi
 
 class Alpaca:
-    def __init__(self, key, secr_key, select_api):
-        self.alpaca_url = select_api
-        self.pub_key = key
-        self.priv_key = secr_key
-
+    def __init__(self):
+        load_dotenv()
+        pub_key = os.getenv('ALPACA_PAPERTRADING_PUB_KEY')
+        priv_key = os.getenv('ALPACA_PAPERTRADING_PRIV_KEY')
+        alpaca_url = os.getenv('ALPACA_PAPERTRADING_URL')
         self.instance = tradeapi.REST(
-            self.pub_key , 
-            self.priv_key,
-            self.alpaca_url
+            pub_key , 
+            priv_key,
+            alpaca_url
         )
+
     def get_assets(self):
         return self.instance.list_assets()
         
-        
+    def get_OHLC(self,url, timeframe):
+        # return
+        test = self.instance.get_barset('AAPL','day')
+        l = 0
     
