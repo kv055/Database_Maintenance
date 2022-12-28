296878dac3977883edf3589d5b0f1e33e98e2780
Kilian Voss <kilian96@live.de>
2022-12-28 15:43:11 +1100

    upper rows just for debugging, will bedeleted later. converted the insert function toasync but doesnt runn concurrently yet, further developement is needed.

--- OHLC_table_updater/Async_update_OHLC.py
+++ OHLC_table_updater/Async_update_OHLC.py
@@ -1,3 +1,4 @@
+import asyncio
 import find_parent
 from OHLC_table_updater.OHLC_db import OHLC_DB
 from OHLC_table_updater.PriceData.Get_OHLC_Class_Async import Import_OHLC_Data_Async
@@ -12,24 +13,31 @@ class update_OHLC_table:
         self.all_asset_dicts = []
 
         
-        dicts = self.ohlc_tables.return_all_asset_dicts_from_dataprovider('Kraken')
-        self.all_asset_dicts.append({
-            'data_provider': 'Kraken',
-            'asset_dicts': dicts
-        })
+        # dicts = self.ohlc_tables.return_all_asset_dicts_from_dataprovider('Kraken')
+        # self.all_asset_dicts.append({
+        #     'data_provider': 'Kraken',
+        #     'asset_dicts': dicts
+        # })
 
-        dicts = self.ohlc_tables.return_all_asset_dicts_from_dataprovider('Binance')
-        self.all_asset_dicts.append({
-            'data_provider': 'Binance',
-            'asset_dicts': dicts
-        })
+        # dicts = self.ohlc_tables.return_all_asset_dicts_from_dataprovider('Binance')
+        # self.all_asset_dicts.append({
+        #     'data_provider': 'Binance',
+        #     'asset_dicts': dicts
+        # })
 
-        # for provider in self.all_data_providers:
-        #     dicts = self.ohlc_tables.return_all_asset_dicts_from_dataprovider(provider['data_provider'])
-        #     self.all_asset_dicts.append({
-        #         'data_provider': provider['data_provider'],
-        #         'asset_dicts': dicts
-        #     })
+
+        # dicts = self.ohlc_tables.return_all_asset_dicts_from_dataprovider('Alpaca')
+        # self.all_asset_dicts.append({
+        #     'data_provider': 'Alpaca',
+        #     'asset_dicts': dicts
+        # })
+
+        for provider in self.all_data_providers:
+            dicts = self.ohlc_tables.return_all_asset_dicts_from_dataprovider(provider['data_provider'])
+            self.all_asset_dicts.append({
+                'data_provider': provider['data_provider'],
+                'asset_dicts': dicts
+            })
 
         config = {
             'Candle_Size': '1_Day',
@@ -46,9 +54,9 @@ class update_OHLC_table:
             Pricedata = Import_OHLC_Data_Async(configuration, self.all_asset_dicts)
             asset_dicts_with_ohlc = Pricedata.OHLC_Price_List_for_DB()
 
-            for index,asset in enumerate(asset_dicts_with_ohlc):
+            async def insert(asset,index):
                 if len(asset['OHLC'])>0:
-                 # Get First and last date of the dataset
+                # Get First and last date of the dataset
                     self.ohlc_tables.insert_first_and_last_date_into_assets_table(
                         asset,
                         asset['OHLC'][0][0],
@@ -63,7 +71,10 @@ class update_OHLC_table:
                     self.ohlc_tables.delete_row_by_data_provider_and_ticker(
                         asset
                     )
-                    print('could not fetch for now deleted selected asset: ',asset['data_provider'],asset['ticker'])
+                    print('Could not fetch, deleted: ',asset['data_provider'],asset['ticker'],index)
+            
+            async def main():
+                tasks = [insert(asset, index) for index,asset in enumerate(asset_dicts_with_ohlc)]
+                await asyncio.gather(*tasks)
 
-hure = update_OHLC_table()
-hure.fetch_OHLC_from_API_and_update_table()
+            asyncio.run(main())
